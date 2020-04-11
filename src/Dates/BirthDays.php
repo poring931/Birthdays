753e73dfa67723c10740ff7c908fb840125982b5
Maxim Gusakov <poring.m@mail.ru>
2020-04-11 21:02:23 +0000

    Refactored to Birthdays class

---
+++ src/Dates/BirthDays.php
@@ -0,0 +1,21 @@
+<?php
+namespace Gusakov\Dates;
+
+class BirthDays
+{
+    private $birthDate;
+    private $todayDate;
+    public function __construct($birthDate) {
+        $this ->birthDate = $birthDate;
+        $this ->todayDate = date("Y-m-d");
+    }
+
+    public function findTotalDays() {
+        $birthTime = strtotime($this->birthDate);
+        $todayTime = strtotime($this->todayDate);
+
+        $totalsecs = $todayTime - $birthTime;
+        $totalDays = floor($totalsecs / 3600 / 24);
+        return $totalDays;
+    }
+}
